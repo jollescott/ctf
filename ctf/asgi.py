d80dee882e3cac43523343b5b74dfd64729b378b
Joel Linder <joelsc.linder@gmail.com>
2024-08-27 22:42:02 +0200

    Installed django channels and daphne
    Adjusted configuration for daphne
    
    Added asgi routing configuration

--- ctf/asgi.py
+++ ctf/asgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctf.settings")
 
-application = get_asgi_application()
+asgi_application = get_asgi_application()
+
+from main.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": asgi_application,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        )
+    }
+)
